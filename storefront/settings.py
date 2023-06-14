0f76158dce7485a01472e0a9ccdb7e985378a86f
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-06-14 07:05:37 +0530

    install mysql databse and makemigrations for core and store app

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'store',
+    
 ]
 
 MIDDLEWARE = [
@@ -78,8 +81,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront_new_4',
+        'PASSWORD':'1234',
+        'PORT': '3310',
+        'HOST':'localhost',
+        'USER':'root',
     }
 }
 
