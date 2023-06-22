2e081d24db9c5e931148737f51b0e8db75e081da
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-06-22 09:44:16 +0530

    make product serializer and set defaultpagination

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Collection, Product, Customer
 
-# Register your models here.
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone', 'membership']
+    list_select_related = ['user']
+
+    def first_name(self, customer):
+        return customer.user.first_name
+    
+    def last_name(self, customer):
+        return customer.user.last_name
--- store/views.py
+++ store/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from .models import Collection
+from rest_framework.response import Response
+from .models import Collection, Product
 from .serializers import CollectionSerializer
 
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count()>0:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
+
+
 
 
