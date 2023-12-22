35dc281d44548ed517befe7ea863cf83b34cad16
Dinindu Tharuka <dinidutharuka@gmail.com>
2023-12-22 09:15:04 +0530

    initiate

--- storefront/settings.py
+++ storefront/settings.py
@@ -88,14 +88,19 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# 'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'storefront_new_4',
+#         'PASSWORD':'1234',
+#         'PORT': '3310',
+#         'HOST':'localhost',
+#         'USER':'root',
+#     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront_new_4',
-        'PASSWORD':'1234',
-        'PORT': '3310',
-        'HOST':'localhost',
-        'USER':'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
